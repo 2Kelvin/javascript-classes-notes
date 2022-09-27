9644fdd928707565e3f079e30f1a0af8ae83a467
2Kelvin <kvin.mwangi254@gmail.com>
2022-09-27 23:29:02 +0300

    learning js classes

---
+++ classes.js
@@ -0,0 +1,11 @@
+// classes create objects -->  i.e. objects are derived from classes
+// an object created from a class is called an instance
+// an object instance has properties/state and methods/functions
+// a class' constructor only runs once; when creating the object 
+//  a constructor is just amethod used to setup objects created by its class
+// every class has a constructor 
+class Rectangle {
+    constructor() {
+
+    }
+}
